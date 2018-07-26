431dfad6736b038c3b2beec1f87ed399b84e681d
Snail <892333187@qq.com>
2018-07-26 23:03:53 +0800

    1. 整合vue前端页面

--- config/app.php
+++ config/app.php
@@ -178,9 +178,10 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         /**
-         * custom
+         * custom add
          */
-        Barryvdh\Debugbar\ServiceProvider::class
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class
     ],
 
     /*
@@ -229,7 +230,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class
     ],
 
 ];
