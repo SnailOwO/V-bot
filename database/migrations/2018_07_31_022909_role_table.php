7337c137de3b93292f9830b83751527f6d3805d9
SnailOwO <892333187@qq.com>
2018-07-31 16:55:51 +0800

    1. 增加laravel permissons

--- database/migrations/2018_07_31_022909_role_table.php
+++ database/migrations/2018_07_31_022909_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class RoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        //
         Schema::create('user', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',32)->default('')->comment('用户名称');
-            $table->string('password',35)->default('')->comment('密码：md5格式');  
+            $table->string('name',32)->default('')->comment('角色名称');
+            $table->text('menu')->default('')->comment('menu');  
             $table->string('email',64)->default('')->comment('邮箱地址');
             $table->char('phone',11)->default('')->comment('手机号(仅支持大陆)');
             $table->text('app_id')->default('')->comment('WX APP ID');
@@ -37,6 +38,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        //
         Schema::dropIfExists('user');
     }
 }
